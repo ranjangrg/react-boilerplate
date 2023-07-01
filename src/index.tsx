e97465461d07399ba349d70136cbda3086ebbcde
Ranjan <ranjan_grg@hotmail.co.uk>
2023-07-01 21:49:14 +0100

    Code changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,102 @@
-import React from "react";
+import React, { MouseEventHandler, ReactEventHandler, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { PanelData } from "./PanelAppData";
-import PanelAppContext from "./PanelAppContext";
-
-import PanelViewer from "./PanelViewer";
 
 import "./index.css";
 
+const defAppContextVal = {};
+const AppContext = React.createContext(defAppContextVal);
+
+const panels = [
+	[25,25,385,405], [385,20,530,405], [545,15,875,400], [15,415,385,805]	// img A
+	//[20,15,100,90], [230,100,330,190], [340,20,580,290]	// img B
+];
+
+enum ViewType { Panel, Page };
+
 function App(props: any) {
-	const panelAppContext = React.useContext(PanelAppContext);
-	const [panelAppState, setCurrentIdx] = React.useState(panelAppContext.state);
-	const changePanel = (newIdx: number) => {
-		let newState = {...panelAppState};
-		newState.currentIdx = newIdx;
-		setCurrentIdx(newState);
+	const appContext = React.useContext(AppContext);
+	const canvasElemRef = React.useRef(document.getElementById("") as HTMLDivElement);
+	const imgElemRef = React.useRef(document.getElementById("") as HTMLImageElement);
+	const [appState, setAppState] = React.useState(null);
+	const [canvasDims, setCanvasDims] = React.useState({ h: 0, w: 0 });
+	const [imgDims, setImgDims] = React.useState({ h: 0, w: 0 });
+	const [imgNatDims, setImgNatDims] = React.useState({ h: 0, w: 0 });
+	const [imgResizeObsv, setImgResizeObsv] = React.useState(new ResizeObserver(() => { }));
+	const [currView, setViewType] = React.useState(ViewType.Page);
+
+	const imgLoadHandler = () => {
+		const canvasElem: HTMLDivElement = canvasElemRef.current;
+		const imgElem: HTMLImageElement = imgElemRef.current;
+		let newCanvasDims = {
+			h: canvasElem.clientHeight,
+			w: canvasElem.clientWidth
+		};
+		let natDims = {
+			h: imgElem.naturalHeight,
+			w: imgElem.naturalWidth
+		};
+		setImgNatDims(natDims);
+		setCanvasDims(newCanvasDims);
+		imgResizeObsv.observe(imgElem);
 	};
-	return (<PanelAppContext.Provider value={{state: panelAppState, changePanel}}><div className="comic-app-container">
-		<h1> App title </h1>
-		<PanelViewer />
+	useEffect(() => {
+		const _imgResizeObsv = new ResizeObserver((entries: ResizeObserverEntry[], observer: ResizeObserver) => {
+			const imgElem: HTMLImageElement = imgElemRef.current;
+			let currDims = {
+				h: imgElem.clientHeight,
+				w: imgElem.clientWidth
+			};
+			setImgDims(currDims);
+		});
+		setImgResizeObsv(_imgResizeObsv);
+	}, []);
+
+	const getScale = (): number => {
+		return imgDims.h / imgNatDims.h;
+	};
+
+	const toggleView = (e: React.MouseEvent) => {
+		if (currView === ViewType.Page) {
+			const areaElem = e.target as HTMLAreaElement;
+			// only do this for <area> components not <img>
+			if (areaElem.coords == null) { return; }
+			const pd = areaElem.coords.split(",");
+			setViewType(ViewType.Panel);
+			const midPointX = (Number(pd[0]) + Number(pd[2])) / 2;
+			const midPointY = (Number(pd[1]) + Number(pd[3])) / 2;
+			imgElemRef.current.style.scale = "2";
+			imgElemRef.current.style.transformOrigin = `${midPointX}px ${midPointY}px`;
+		} else {
+			setViewType(ViewType.Page);
+			const newScale = imgNatDims.h / canvasDims.h
+			imgElemRef.current.style.scale = `${newScale}`;
+		}
+	};
+
+	return (<AppContext.Provider value={{}}><div className="comic-app-container">
+		<h1> App title: {currView === ViewType.Page ? "Page" : "Panel"} view</h1>
+		<div>
+			<span> Canvas: ({canvasDims.h}, {canvasDims.w}) </span>
+			<span> Original: ({imgNatDims.h}, {imgNatDims.w}) </span>
+			<span> Current: ({imgDims.h}, {imgDims.w}) </span>
+			<span> Scale: {getScale()} </span>
+		</div>
+		<div className="img-container" ref={canvasElemRef}>
+			<img onLoad={imgLoadHandler} ref={imgElemRef}
+				onClick={toggleView}
+				useMap="#panels-grid-map" src="/images/catman.jpg"></img>
+			<map name="panels-grid-map">
+				{panels.map((pd, idx) => {
+					let normalisedCoords = pd.map((cd) => cd * getScale());
+					return (<area key={idx}
+						shape="rect" coords={normalisedCoords.toString()}
+						onClick={toggleView}
+						title={`Panel-${idx}`} href="#idx" />);
+				})}
+			</map>
+		</div>
 	</div>
-	</PanelAppContext.Provider>);
+	</AppContext.Provider>);
 }
 
 const container = document.getElementById("app-root")!;
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
+      favicon: "./public/favicon.ico"
     }),
     new MiniCssExtractPlugin(),
   ],
